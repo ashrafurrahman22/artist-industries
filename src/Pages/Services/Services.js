69f6474817609eecab586eb56711ef5527fa0760
ashrafur22 <ashrafurrahman22@gmail.com>
2022-06-25 22:40:59 +0600

    ready-for-deploy

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import gifhy from '../../../assets/cooking.gif';
+// import gifhy from '../../../assets/cooking.gif';
 
 const Services = () => {
     return (
@@ -7,7 +7,7 @@ const Services = () => {
             <h2 className="text-2xl text-amber-400 font-semibold my-5 ml-16 uppercase">Coming Soon...</h2>
             <div className='flex justify-center
             items-center'>
-                <img src={gifhy} alt="" />
+                <img src="https://i.pinimg.com/originals/ef/69/cd/ef69cd230963b34100b23f7fdf5c47eb.gif" alt="" />
             </div>
         </div>
     );
